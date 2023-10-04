0446dc1d7ea2d1fe5bb3605a1edd8dc3d63537a5
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2023-10-04 00:16:58 -0400

    Add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+const gameOptions = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+  let computerChoice = gameOptions[Math.floor(Math.random() * gameOptions.length)];
+  return computerChoice;
+}
+
+function playRound() {
+  
+}
