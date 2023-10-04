af53b8e4feca6bc9c612219d78d475abd8ed7c3d
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2023-10-04 01:09:51 -0400

    Add full gameplay function for 5 rounds

--- script.js
+++ script.js
@@ -21,11 +21,42 @@ function getPlayerChoice() {
 
 function playRound(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
-    alert(`Both you and the computer played '${playerChoice}'. Round is a tie!`)
+    alert(`Both you and the computer played '${playerChoice}'. Round is a tie!`);
+    return "Tie";
   } else if ((playerChoice === "Rock" && computerChoice === "Scissors") || (playerChoice === "Paper" && computerChoice === "Rock") || (playerChoice === "Scissors" && computerChoice === "Paper")) {
-    alert(`You played '${playerChoice}' and the computer played '${computerChoice}'. You win this round!`)
+    alert(`You played '${playerChoice}' and the computer played '${computerChoice}'. You win this round!`);
+    return "Player Win";
   } else {
-    alert(`You played '${playerChoice}' and the computer played '${computerChoice}'. You lose this round...`)
+    alert(`You played '${playerChoice}' and the computer played '${computerChoice}'. You lose this round...`);
+    return "Computer Win";
   }
 }
 
+function game() {
+  alert("Welcome to 'Rock, Paper, Scissors'! Gameplay will last 5 rounds.")
+
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 1; i <= 5; i++) {
+    alert(`Round ${i}       (Player: ${playerScore} || Computer: ${computerScore})`);
+
+    let roundResult = playRound(getPlayerChoice(), getComputerChoice());
+    if (roundResult === "Player Win") {
+      playerScore += 1;
+    } else if (roundResult === "Computer Win") {
+      computerScore += 1;
+    }
+  }
+
+  if (playerScore > computerScore) {
+    alert(`Congratulations! You won ${playerScore} - ${computerScore}`);
+  } else if (computerScore > playerScore) {
+    alert(`Nice Try! You lost ${playerScore} - ${computerScore}`);
+  } else {
+    alert(`Wow! You tied ${playerScore} - ${computerScore}`)
+  }
+
+}
+
+game();
